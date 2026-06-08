383bee9493984c526582f2e05dcb19a5c8f9ed76
monikabaabu <monikababu207@gmail.com>
2026-06-08 16:53:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router'
+import {HomePage} from './pages/HomePage'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="checkout" element={<div>Checkout</div>} />
+    </Routes>
+  )
+}
+
+export default App
